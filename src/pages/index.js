8dce93eff3bee1e5703c21a9c53a9caf67bb9698
jest-in <jestin9900767795@gmail.com>
2023-06-20 21:18:31 +0530

    hamberger functionalities added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavHashLink as Link } from "react-router-hash-link";
 
 import boy1 from "../assets/boy-1.png";
@@ -11,37 +11,47 @@ import hamberger from "../assets/hamberger-open.svg";
 import hambergerClose from "../assets/hamberger-close.svg";
 
 export default function Index() {
+  const [hambergerState, setHambergerState] = useState(window.screen.width<=576?'':'hidden');
+  function toggleHambergerState(){
+    setHambergerState(hambergerState?'':'hidden');
+  }
+  useEffect(()=>{
+    window.addEventListener('blur',()=>setHambergerState(''));
+  });  
   return (
     <div className="index-container">
       <header className="index-header">
-        <Link to="/">
+        <Link onClick={()=>setHambergerState('')} to="/">
           <img className="index-logo-black" src={logoBlack} alt="logo" />
         </Link>
-        <nav className="index-nav-bar">
-          <button className="hamberger-open">
-            <img src={hamberger} alt="hamberger" />
-          </button>
-          <button className="hamberger-close">
+        <button
+          className={`hamberger-open ${hambergerState}`}
+          onClick={toggleHambergerState}
+        >
+          <img src={hamberger} alt="hamberger" />
+        </button>
+        <nav className={`index-nav-bar ${hambergerState?'':'hidden'}`}>
+          <button className="hamberger-close" onClick={toggleHambergerState}>
             <img src={hambergerClose} alt="hamberger-close" />
           </button>
           <ul className="index-nav-links">
             <li>
-              <Link className="index-nav" to="/">
+              <Link onClick={()=>setHambergerState('')} className="index-nav" to="/">
                 Home
               </Link>
             </li>
             <li>
-              <Link className="index-nav" to="#catalog">
+              <Link onClick={()=>setHambergerState('')} className="index-nav" to="#catalog">
                 Catalog
               </Link>
             </li>
             <li>
-              <Link className="index-nav" to="#about-us">
+              <Link onClick={()=>setHambergerState('')} className="index-nav" to="#about-us">
                 About Us
               </Link>
             </li>
             <li>
-              <Link className="index-nav" to="#contact-us">
+              <Link onClick={()=>setHambergerState('')} className="index-nav" to="#contact-us">
                 Contact Us
               </Link>
             </li>
@@ -53,15 +63,17 @@ export default function Index() {
         <div className="hero-sec">
           <aside>
             <p className="index-hero-caption">
-              <span>Discover a world of</span> <span className="index-hero-caption-sec1">POSSIBILITIES</span> <span>where every desire finds its</span> <span className="index-hero-caption-sec2">PERFECT MATCH</span>
+              <span>Discover a world of</span>{" "}
+              <span className="index-hero-caption-sec1">POSSIBILITIES</span>{" "}
+              <span>where every desire finds its</span>{" "}
+              <span className="index-hero-caption-sec2">PERFECT MATCH</span>
             </p>
           </aside>
 
           <img className="index-img-1" src={boy1} alt="boy-1" />
         </div>
-        
+
         <section id="catalog">
-          
           <img className="index-img-2" src={boy2} alt="boy-2" />
           <div className="index-catalog-container">
             <div className="index-catalog-div1">
@@ -90,26 +102,27 @@ export default function Index() {
 
         <section id="about-us">
           <div className="about-us-container">
-          <h2 className="section-heading-aboutus">ABOUT US</h2>
-          <div className="catalog-about-us-content">
-            <p>
-              Discover a world of convenience and style at Esadhi.com. We are your
-              premier online shopping destination, offering a wide range of
-              products across categories such as fashion, electronics, home decor,
-              and more. With a commitment to quality and customer satisfaction, we
-              strive to provide you with a seamless shopping experience. Our
-              curated collection features trusted brands and exquisite designs,
-              ensuring that you find exactly what you're looking for. At
-              esadhi.com, we prioritize your needs and offer dedicated customer
-              support to assist you at every step. Join us today and embark on a
-              journey of unparalleled convenience and endless possibilities.
-            </p>
-            <p className="about-us-para2">
-              Thank you for choosing esadhi.com as your preferred destination for
-              all your shopping desires. Shop with us and let us redefine your
-              online shopping experience.
-            </p>
-          </div>
+            <h2 className="section-heading-aboutus">ABOUT US</h2>
+            <div className="catalog-about-us-content">
+              <p>
+                Discover a world of convenience and style at Esadhi.com. We are
+                your premier online shopping destination, offering a wide range
+                of products across categories such as fashion, electronics, home
+                decor, and more. With a commitment to quality and customer
+                satisfaction, we strive to provide you with a seamless shopping
+                experience. Our curated collection features trusted brands and
+                exquisite designs, ensuring that you find exactly what you're
+                looking for. At esadhi.com, we prioritize your needs and offer
+                dedicated customer support to assist you at every step. Join us
+                today and embark on a journey of unparalleled convenience and
+                endless possibilities.
+              </p>
+              <p className="about-us-para2">
+                Thank you for choosing esadhi.com as your preferred destination
+                for all your shopping desires. Shop with us and let us redefine
+                your online shopping experience.
+              </p>
+            </div>
           </div>
           <img className="index-img-3" src={boy3} alt="boy-3" />
         </section>
